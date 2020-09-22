7fd59886f040edbf435806de4379c3b7ed9f0fa4
Jerry cheng <imjcroot@gmail.com>
2020-09-22 17:52:57 +0800

    test 1 testbranch

--- .history/PRACTICE/TEST/test_20200922175221.js
+++ .history/PRACTICE/TEST/test_20200922175221.js
@@ -2,4 +2,4 @@ let age = prompt('How old are you?', 100);
 alert(`You are ${age} years old!`);
 
 let age = prompt('How old are you?', '');
-alert(`You are ${age} years old!`);
+        alert(`You are ${age} years old!`);
